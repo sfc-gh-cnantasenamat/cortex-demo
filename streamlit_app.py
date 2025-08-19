2ce79080caa37340262116ac56c086a1fb3e8350
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-08-18 23:11:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-#import pandas as pd
-#import matplotlib.pyplot as plt
-#from snowflake.cortex import complete
+import pandas as pd
+import matplotlib.pyplot as plt
+from snowflake.cortex import complete
 
 st.title("Hello")
