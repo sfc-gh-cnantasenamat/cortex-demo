c9eef74da0da21d389e045530828f4a456c01bda
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-09-25 11:57:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(
 )
 
 pages = {
-        "Home": st.Page("text_app.py", title="Cortex Text example", icon=":material/description:"),
+        "Home": [st.Page("text_app.py", title="Cortex Text example", icon=":material/description:")],
         "Cortex Text Demo": [
             st.Page("text_app.py", title="Cortex Text example", icon=":material/description:"),
             st.Page("text_minimal_app.py", title="Cortex Text (minimal) example", icon=":material/notes:"),
