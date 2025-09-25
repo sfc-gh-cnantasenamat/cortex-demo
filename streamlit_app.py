facbc4e5f29e31022ddbf6d7f08ee67e57b40ec4
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2025-09-25 13:33:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,11 @@ cortex_demo_pages = [
 ]
 
 # Define the full navigation structure
-pages = [
+pages = {
     # A standalone page for Home, set as the default
-    st.Page("text_app.py", title="Home", icon=":material/home:"),
+    # st.Page("text_app.py", title="Home", icon=":material/home:"),
     "Cortex Text Demo": cortex_demo_pages
-]
+}
 
 # Create the navigation from the list of pages
 pg = st.navigation(pages, position="top")
